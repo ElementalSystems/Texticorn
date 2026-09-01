1657ce9cf1a976c1b83d275f9476bb1563d9ea41
Andrew HS <ahigsonsmith@gmail.com>
2026-09-01 17:27:27 +0200

    First rainbow

--- js/play.js
+++ js/play.js
@@ -1,20 +1,82 @@
+//Levels
+
+const levs = [
+  {
+    map: myMap,
+    //set colours?
+    title: "RED - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+  {
+    map: myMap,
+    //set colours?
+    title: "ORANGE - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+  {
+    map: myMap,
+    //set colours?
+    title: "Yellow - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+  {
+    map: myMap,
+    //set colours?
+    title: "green - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+  {
+    map: myMap,
+    //set colours?
+    title: "blue - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+  {
+    map: myMap,
+    //set colours?
+    title: "indigo - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+  {
+    map: myMap,
+    //set colours?
+    title: "Rviolet - INTRO",
+    time4: 30,
+    time5: 18,
+  },
+];
+
+buildLevelMenu = () => {
+  const p = document.getElementById("menu");
+  levs.forEach((l, i) => {
+    const ctl = document.createElement("div");
+    ctl.classList = "lev";
+    ctl.textContent = l.title;
+    p.appendChild(ctl);
+  });
+};
+
 //TODO: The List
 /*
  * Design
  *  * Level design
- *    * 1 - Intro Jumps and controls and curves - prizes and exit
+ *    * 1 - Intro Jumps and controls and curves - prizes and exit (3)
  *    * 2 - The flip side and the solid bits
  *
  *  * more unicorn
- *   * Decide on frame definitions: (1)
- *       * back crouch, streched out, streched out (A), front crouch, walk, streched out, streched out (A), hunched together
- *   * Get moves to look good (and check empty spaces)
+ *   * moves (and check empty spaces) (1)
  *
  * Map
  *
- * Technical]
- *    allow limb offset f and r classes
- *  * arch 3d thing (4)
+ * Technical
+ *  * arch 3d thing (2)
+ *  * speech bubble (control the curve text - no layers)
  *
  *
  * game
@@ -22,16 +84,14 @@
  *   the ((rainbow))
  *   level menu
  *
- *  Sprite:
- *    z-offset on legs
- *    speech bubble (control the curve text - no layers)
+ 
  */
 
 async function start() {
+  buildLevelMenu();
   //const controls = mkCtls(document.getElementById("ctls"));
   //play(myMap, controls);
-
-  showSprite(uni, "i");
+  //showSprite(uni, "fd tb fd tb u2 tb d2 fd");
 }
 
 function play(mapC, controls) {
@@ -44,15 +104,16 @@ function play(mapC, controls) {
 async function showSprite(config, mn) {
   const map = mkMap(mapD, null, document.getElementById("scene"));
   //addGridDisplay(map);
-  map.base.style.setProperty("--viewS", 1);
+  map.base.style.setProperty("--viewS", 0.75);
 
-  map.setCamera(3, 3, 0);
-  config.bits[0].pos.forEach((_, i) => addSprite(config, map, i + 1, 2, 0, i));
+  map.setCamera(4, 3, 0);
+  config.bits[0].pos.forEach((_, i) => addSprite(config, map, i + 1, 1, 0, i));
   if (mn) {
-    let t = addSprite(config, map, 1, 4);
+    let t = addSprite(config, map, 1, 1);
     while (true) {
-      t.setP(1, 4, 100, 0);
-      await t.move(mn);
+      t.setP(1, 2, 100, 0);
+      let mns = mn.split(" ");
+      for (let i = 0; i < mns.length; i += 1) await t.move(mns[i]);
     }
   }
 }
