7387c45e183d24d5e0c97b7a95627ecd287eacb8
Andrew HS <ahigsonsmith@gmail.com>
2026-09-05 01:28:05 +0200

    New demo levels camera and stuff

---
+++ demos/day22/js/menu.js
@@ -0,0 +1,28 @@
+buildLevelMenu = (p, sl) => {
+  levs.forEach((l, i) => {
+    const ctl = document.createElement("div");
+    ctl.classList = "lev";
+    ctl.textContent = l.title;
+    ctl.onclick = () => {
+      sl(i);
+    };
+    p.appendChild(ctl);
+  });
+  return {
+    show: () => p.classList.toggle("hide", false),
+    hide: () => p.classList.toggle("hide", true),
+  };
+};
+
+buildScreen = () => {
+  const menu = buildLevelMenu(document.getElementById("menu"), (li) => {
+    ctls.startUp(li);
+    menu.hide();
+  });
+  menu.show();
+
+  const ctls = mkCtls(document.getElementById("ctls"), () => {
+    //end of game status
+    menu.show();
+  });
+};
